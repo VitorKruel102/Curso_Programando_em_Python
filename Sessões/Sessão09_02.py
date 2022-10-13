03226b50b79b4aabfb1a17c5affc078c3cc99ca7
Vitor Kruel102 <vkruel.programador@gmail.com>
2022-10-13 11:40:36 -0300

    Aula 56 - Dictionaty Comprehension.

--- Sessões/Sessão09_02.py
+++ Sessões/Sessão09_02.py
@@ -1,15 +1,12 @@
 """
 ===============================================
 
-            List Comprehension
+            Dictionaty Comprehension
 
 ===============================================
 """
-numeros = [1, 2, 3, 4, 5]
+numeros = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
 
-pares = [numero for numero in numeros if not numero % 2]
-impares = [numero for numero in numeros if numero % 2]
-res = [numero * 2 if numero % 2 == 0 else numero / 2 for numero in numeros]
+quadrado = {chave: valor ** 2 for chave, valor in numeros.items()}
 
-print(pares)
-print(impares)
+print(quadrado)
