d6558d8354eb3e54cf812fa0cdaa66da6b262019
Vitor Kruel102 <vkruel.programador@gmail.com>
2022-10-18 13:48:22 -0300

    Aula sobre sorted.

---
+++ Sessões/Sessão10_07.py
@@ -0,0 +1,17 @@
+"""
+===============================================
+
+                    Sorted
+
+===============================================
+"""
+
+list_numeros = [6, 1, 8, 2]
+set_numeros = {6, 1, 8, 2}
+
+
+print(sorted(list_numeros)) # Retorna sempre uma lista.
+print(sorted(set_numeros))  # Retorna sempre uma lista.
+
+print(sorted(list_numeros, reverse=True)) # Maior para o menor
+print(sorted(set_numeros, reverse=True))  # Maior para o menor
