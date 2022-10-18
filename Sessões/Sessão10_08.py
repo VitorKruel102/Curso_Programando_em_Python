dbac26e96465b201c4c092b55bb7c1c1180651ec
Vitor Kruel102 <vkruel.programador@gmail.com>
2022-10-18 14:13:34 -0300

    Aula sobre reversed.

---
+++ Sessões/Sessão10_08.py
@@ -0,0 +1,13 @@
+"""
+===============================================
+
+                Reversed
+
+===============================================
+"""
+
+list_numeros = [6, 1, 8, 2]
+
+print(list(reversed(list_numeros))) # Retorna sempre uma lista.
+print(set(reversed(list_numeros))) # Retorna sempre uma lista.
+print(tuple(reversed(list_numeros))) # Retorna sempre uma lista.
